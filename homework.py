b7828cdadab4db368add0180a4ed8211eb4732a3
Airat Sakhibgareev <mr-airat@yandex.ru>
2022-04-03 20:58:15 +0300

    2nd review

--- homework.py
+++ homework.py
@@ -21,17 +21,12 @@ ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
 
-HOMEWORK_STATUSES = {
+HOMEWORK_VERDICTS = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
     'reviewing': 'Работа взята на проверку ревьюером.',
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
-
-logging.basicConfig(
-    filename='main.log',
-    format='%(asctime)s [%(levelname)s] %(message)s'
-)
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -57,17 +52,17 @@ def get_api_answer(current_timestamp):
     """
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
-    response = requests.get(ENDPOINT, headers=HEADERS, params=params)
+    response = requests.get(url=ENDPOINT, headers=HEADERS, params=params)
     try:
+        response
         if response.status_code == 200:
             return response.json()
         message = 'Эндпоинт недоступен'
         logger.error(message)
         raise ValueError(message)
-    except Exception:
+    except telegram.TelegramError:
         message = 'Наблюдаются сбои при запросе к эндпоинту'
         logger.error(message)
-        raise ValueError(message)
 
 
 def check_response(response):
@@ -75,11 +70,15 @@ def check_response(response):
     Возвращает список домашних работ из ответа API.
     """
     homeworks_list = response['homeworks']
-    if 'homeworks' in response:
-        if isinstance(homeworks_list, list):
-            return homeworks_list
+    if not isinstance(response, dict):
+        raise ValueError('Ответ API представлен не в виде словаря')
+    if not bool(response):
+        raise ValueError('Ответ API пришел в виде пустого словаря')
+    if not isinstance(homeworks_list, list):
         raise ValueError('Домашние задания представлены не в виде списка')
-    return False
+    if 'homeworks' not in response:
+        return False
+    return homeworks_list
 
 
 def parse_status(homework):
@@ -89,57 +88,64 @@ def parse_status(homework):
     homework_name = homework['homework_name']
     homework_status = homework['status']
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    if all((homework_name, homework_status)) is True:
-        if homework_status in HOMEWORK_STATUSES:
-            verdict = HOMEWORK_STATUSES[homework_status]
+    try:
+        all((homework_name, homework_status))
+        if homework_status in HOMEWORK_VERDICTS:
+            verdict = HOMEWORK_VERDICTS[homework_status]
             message = (f'Изменился статус проверки работы "{homework_name}". '
                        f'{verdict}')
             return message
         message = f'Статус {homework_status} недокументирован'
         logger.error(message)
-        send_message(bot, message)
+        bot.send_message(TELEGRAM_CHAT_ID, message)
         raise ValueError(message)
-    message = 'Ключи "homework_name" и "status" в списке отсутствуют'
-    logger.error(message)
-    send_message(bot, message)
-    raise ValueError(message)
+    except Exception:
+        message = 'Ключи "homework_name" и "status" в списке отсутствуют'
+        logger.error(message)
+        bot.send_message(TELEGRAM_CHAT_ID, message)
+        raise ValueError(message)
 
 
-def check_tokens():
+def check_tokens() -> bool:
     """Проверяет доступность переменных окружения."""
-    all_tokens = all((PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID))
-    if all_tokens is True:
-        return True
-    else:
-        logger.critical('Переменные окружения отсутствуют')
-        return False
+    return all((PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID))
 
 
 def main():
     """Основная логика работы бота."""
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
-    check_tokens()
+    initial_message = ''
+    if check_tokens() is False:
+        logger.critical('Переменные окружения отсутствуют')
     while True:
         try:
             response = get_api_answer(current_timestamp)
             homework = check_response(response)
             if homework is not False:
-                message = parse_status(homework)
+                current_homework = homework[0]
+                message = parse_status(current_homework)
             else:
                 message = 'В списке отсутствует ключ "homeworks"'
                 logger.error(message)
-            send_message(bot, message)
+            if message == initial_message:
+                pass
+            else:
+                initial_message = message
+                bot.send_message(TELEGRAM_CHAT_ID, initial_message)
             current_timestamp = int(time.time())
             time.sleep(RETRY_TIME)
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            send_message(bot, message)
-            current_timestamp = int(time.time())
+            bot.send_message(TELEGRAM_CHAT_ID, message)
             time.sleep(RETRY_TIME)
         else:
             pass
 
 
 if __name__ == '__main__':
+    logging.basicConfig(
+        filename='main.log',
+        format='%(asctime)s [%(levelname)s] %(message)s'
+    )
     main()
